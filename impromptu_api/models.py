8f599076bdfafc22ed8e8cd260e0d8f6e07501c7
slye20 <samuellim2011@gmail.com>
2024-05-06 20:44:02 +0800

    Created get and post requests for listings

--- impromptu_api/models.py
+++ impromptu_api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length = 100)
+    description = models.TextField()
+    location = models.CharField(max_length = 180)
+    timestamp = models.DateTimeField(auto_now_add = True, auto_now = False, blank = True)
+    updated = models.DateTimeField(auto_now = True, blank = True)
+    user = models.ForeignKey(User, on_delete = models.CASCADE, blank = True, null = True)
+
+    def __str__(self):
+        return self.task
--- impromptu_api/urls.py
+++ impromptu_api/urls.py
@@ -2,9 +2,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from impromptu_api import urls as impromptu_urls
+from .views import (ImpromptuList)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-    path('impromptu/', include(impromptu_urls)),
+    # path('admin/', admin.site.urls),
+    # path('api-auth/', include('rest_framework.urls')),
+    # path('impromptu/', include(impromptu_urls)),
+    path('api/', ImpromptuList.as_view(), name='impromptu-list')
 ]
