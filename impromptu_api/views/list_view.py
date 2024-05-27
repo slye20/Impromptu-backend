a9590668f19ec120a20df652521e9367ac092d88
slye20 <samuellim2011@gmail.com>
2024-05-27 22:16:06 +0800

    Added CRUD operations

--- impromptu_api/views/list_view.py
+++ impromptu_api/views/list_view.py
@@ -2,8 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import permissions
-from .models import Post
-from .serializers import ImpromptuSerializer
+from ..models import Post
+from ..serializers import ImpromptuSerializer
 
 class ImpromptuList(APIView):
     # permission_classes = [permissions.IsAuthenticated]
