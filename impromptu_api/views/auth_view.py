c5952c577071f8112188b99268bce63ac2115e48
slye20 <samuellim2011@gmail.com>
2024-06-04 22:19:02 +0800

    updated README.me

--- impromptu_api/views/auth_view.py
+++ impromptu_api/views/auth_view.py
@@ -12,7 +12,7 @@ from django.shortcuts import get_object_or_404
 def login(request):
     user = get_object_or_404(User, username=request.data['username'])
     if not user.check_password(request.data['password']):
-        return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+        return Response({"detail": "Invalid username or password."}, status=status.HTTP_401_UNAUTHORIZED)
     token, created = Token.objects.get_or_create(user=user)
     serializer = UserSerializer(instance=user)
     return Response({"token": token.key, "user": serializer.data}, status=status.HTTP_200_OK)
